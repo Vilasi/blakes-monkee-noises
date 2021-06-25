5f8d877eba10359fc16a9e213288b464e1f986a1
Vilasi <VilasiCoding@gmail.com>
2021-06-24 23:18:26 -0400

    Added Play All button

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const monkeeList = document.querySelector('.monkee-noises-list');
+const playAll = document.querySelector('.btn');
 
 const monkeeNoisesArray = [];
 
@@ -8,9 +9,17 @@ for (let i = 1; i < 3; i++) {
 
 monkeeList.innerHTML = '';
 
+// generate audio <li> elements
 monkeeNoisesArray.forEach((num) => {
   console.log(num);
   monkeeList.innerHTML += `<li class="list-group-item text-center">
           <audio controls><source src="audio/${num}.mp3" type="audio/mpeg" /></audio>
         </li>`;
+});
+
+// play all audio at once
+playAll.addEventListener('click', (event) => {
+  let monkees = document.querySelectorAll('audio');
+
+  monkees.forEach((monke) => monke.play());
 });
